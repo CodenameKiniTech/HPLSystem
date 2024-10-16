a29284f690ba90f8def48c6a402969e1fb6f5a4b
Raymond <raymondcordova88@gmail.com>
2024-10-17 01:01:56 +0800

    Screens Basics

---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+
+const ProductDetailsScreen = () => {
+
+    const { id } = useLocalSearchParams(); 
+    return (
+        <View>
+          <Stack.Screen options={{ title: 'Details ' + id }} />
+            <Text>
+                Gwapo Raymond! {id}
+            </Text>
+        </View>
+    )
+}
+
+export default ProductDetailsScreen;
