80ba154a769e5c525338b20a981ed6e3fa4b7ea2
Raymond <raymondcordova88@gmail.com>
2024-10-21 11:31:46 +0800

    Admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,75 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { PizzaSize } from "@/types";
+import React, { useState } from 'react';
+import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { useRoute } from "@react-navigation/native";
+
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL', 'XXL'];
+
+const ProductDetailsScreen = () => {
+
+    const { id } = useLocalSearchParams();
+    const { addItem } = useCart();
+    const router = useRouter();
+    const [selectedSize, setSelectedSize] = useState<PizzaSize>('S');
+
+    const product = products.find((p) => p.id.toString() == id);
+
+    const addToCart = () => {
+        if (!product) {
+            return;
+        }
+        addItem(product, selectedSize);
+        router.push('/cart');
+    };
+
+    if (!product) {
+        return <Text>Product not found</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{ title: 'Details ' + product.name }} />
+
+            <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+
+            <Text style={styles.title}>
+                {product.name}
+            </Text>
+            <Text style={styles.price}>
+                Price ₱{product.price}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        flex: 1,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+        alignSelf: 'center',
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    subtitle: {
+        marginVertical: 10,
+        fontWeight: '600',
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
+
+export default ProductDetailsScreen;
