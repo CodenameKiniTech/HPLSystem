948fce1e7cdb061ffd58fb14c09791bde7a7b4f8
Raymond <raymondcordova88@gmail.com>
2024-10-25 22:09:26 +0800

    Product CRUD (View All)

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,16 +1,25 @@
-import { View, FlatList } from 'react-native';
-import products from '@assets/data/products';
-import ProductListItem from '@components/ProductListItem';
+import { FlatList, ActivityIndicator, Text } from "react-native";
+import ProductListItem from "@components/ProductListItem";
+import { useProductList } from "@/api/products";
 
 export default function MenuScreen() {
-  return (
-   <FlatList
-     data={products}
-     renderItem={({ item }) => <ProductListItem product={item} />}
-     numColumns={2}
-     contentContainerStyle={{ gap: 10, padding: 10 }}
-     columnWrapperStyle={{ gap: 10 }}
-    />
-  )
-}
+  const { data: products, error, isLoading } = useProductList();
 
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
+}
